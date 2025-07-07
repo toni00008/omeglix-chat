f03189ee987624e8b0990fcf5e7f6f84ba803d64
toni00008 <mdtonmoy6915@gmail.com>
2025-07-07 19:56:13 +0600

    Initial Omeglix chat and video deploy

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.use(express.static('public'));
 
 let users = new Set();
 let waitingUser = null;
+let videoWaitingUser = null;
 
 function updateOnlineCount() {
   io.emit('onlineCount', users.size);
 }
 
-// Helper to get country from IP
+// Get country from IP
 async function getCountryFromIP(ip) {
   try {
     const response = await axios.get(`http://ip-api.com/json/${ip}`);
@@ -37,13 +38,13 @@ io.on('connection', async (socket) => {
   users.add(socket.id);
   updateOnlineCount();
 
+  // TEXT CHAT LOGIC
   if (waitingUser) {
     const partner = waitingUser;
     waitingUser = null;
     socket.partner = partner;
     partner.partner = socket;
 
-    // Send country info to each other
     socket.emit('partner-found', {
       country: partner.country,
       code: partner.countryCode
@@ -98,13 +99,49 @@ io.on('connection', async (socket) => {
     }
   });
 
+  // VIDEO CHAT LOGIC
+  socket.on('video-ready', () => {
+    if (videoWaitingUser && videoWaitingUser !== socket) {
+      socket.partner = videoWaitingUser;
+      videoWaitingUser.partner = socket;
+
+      socket.emit('start-video');
+      videoWaitingUser.emit('start-video');
+
+      videoWaitingUser = null;
+    } else {
+      videoWaitingUser = socket;
+    }
+  });
+
+  socket.on('video-offer', (data) => {
+    if (socket.partner) socket.partner.emit('video-offer', data);
+  });
+
+  socket.on('video-answer', (data) => {
+    if (socket.partner) socket.partner.emit('video-answer', data);
+  });
+
+  socket.on('video-candidate', (data) => {
+    if (socket.partner) socket.partner.emit('video-candidate', data);
+  });
+
+  socket.on('video-next', () => {
+    if (socket.partner) {
+      socket.partner.emit('partner-disconnected');
+      socket.partner.partner = null;
+    }
+
+    socket.partner = null;
+    socket.emit('video-ready');
+  });
+
   socket.on('disconnect', () => {
     users.delete(socket.id);
     updateOnlineCount();
 
-    if (waitingUser === socket) {
-      waitingUser = null;
-    }
+    if (waitingUser === socket) waitingUser = null;
+    if (videoWaitingUser === socket) videoWaitingUser = null;
 
     if (socket.partner) {
       socket.partner.partner = null;
@@ -116,4 +153,3 @@ io.on('connection', async (socket) => {
 http.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
-
